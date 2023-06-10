9e47963606f97c9d639904a24b8cd5de08808f1b
patryjas <patrykjas08@gmail.com>
2023-06-10 21:40:32 +0200

    fix

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -20,9 +21,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  const start = Date.now();
+  res.on('finish', function() {
+    const end = Date.now();
+    const responseTime = end - start;
+    console.log(`Czas odpowiedzi: ${responseTime}ms`);
+  });
+  next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
+app.use('/about', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
