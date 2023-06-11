ce2818838b7695f41985959776adb22ec10c5fb7
student <student@ubuntu224-20.edu-wsb.local>
2023-06-11 10:46:31 +0200

    added static files function

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
 
 app.use(function(req, res, next) {
   const start = Date.now();
